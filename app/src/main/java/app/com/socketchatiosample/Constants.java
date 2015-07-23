5281028e3139ece8c4b5cb7947c9a015b425477b
Angad Tiwari <angtwr31@gmail.com>
2015-07-23 14:29:17 +0530

    Initial commit

---
+++ app/src/main/java/app/com/socketchatiosample/Constants.java
@@ -0,0 +1,6 @@
+package app.com.socketchatiosample;
+
+public class Constants {
+    //public static final String CHAT_SERVER_URL = "http://chat.socket.io";
+    public static final String CHAT_SERVER_URL = "http://192.168.0.59:8080";
+}
